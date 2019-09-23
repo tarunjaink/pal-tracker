ea890a709c0567b0082d69446eeab8a4735e1efd
tarun jain <tarun_jain1@dell.com>
2019-09-23 14:18:05 +0530

    asp.net

---
+++ src/PalTracker/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace PalTracker
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton(sp => new WelcomeMessage(
+               Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")
+           ));
+
+            services.AddSingleton(sp => new CloudFoundryInfo(
+                Configuration.GetValue<string>("PORT", "PORT not set"),
+                Configuration.GetValue<string>("MEMORY_LIMIT", "MEMORY_LIMIT not set"),
+                Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX not set"),
+                Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR not set")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ src/PalTracker/WelcomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+namespace PalTracker
+{
+    [Route("/")]
+    public class WelcomeController : ControllerBase
+    {
+        private readonly WelcomeMessage _message;
+
+        [HttpGet]
+        public string SayHello() => _message.Message;
+
+        public WelcomeController(WelcomeMessage message)
+        {
+            _message = message;
+        }
+    }
+}

bf3644b3f9ebb1e361d8ccb0f2be1ef0390d5026
tarun jain <tarun_jain1@dell.com>
2019-09-24 12:33:04 +0530

    repostiroy test

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -36,7 +37,8 @@ namespace PalTracker
                 Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX not set"),
                 Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR not set")));
 
-            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
